17dbf3e1ba8c4d003c245dc96064b80f68918c2f
faruqfadhil <faruqfadhil7@gmail.com>
2019-02-28 17:01:50 +0700

    change query for requested sapi

--- sensor_server/repositories/sapi.repositories.js
+++ sensor_server/repositories/sapi.repositories.js
@@ -213,9 +213,41 @@ const sapiRepositories = {
     }
   },
   getRequestSapi: async()=>{
-    let result = await Sapi.find({
-      "perangkat.status":Constants.DEVICE_PENDING
-    })
+    let result = await Sapi.aggregate(
+      // Pipeline
+      [
+        // Stage 1
+        {
+          $match: {
+              "perangkat.status":Constants.DEVICE_PENDING
+          
+          }
+        },
+
+        // Stage 2
+        {
+          $lookup: // Equality Match
+          {
+              from: "peternaks",
+              localField: "idPeternak",
+              foreignField: "_id",
+              as: "peternak_docs"
+          }
+          
+          // Uncorrelated Subqueries
+          // (supported as of MongoDB 3.6)
+          // {
+          //    from: "<collection to join>",
+          //    let: { <var_1>: <expression>, …, <var_n>: <expression> },
+          //    pipeline: [ <pipeline to execute on the collection to join> ],
+          //    as: "<output array field>"
+          // }
+        },
+
+      ]
+
+      // Created with Studio 3T, the IDE for MongoDB - https://studio3t.com/
+    )
     return result
   }
 
