f0cc07045126ac452ca8890a2f63d40f2c90a56c
faruqfadhil <faruqfadhil7@gmail.com>
2019-04-09 21:02:52 +0700

    add controller

--- expert_system/modules/app/controllers/label.py
+++ expert_system/modules/app/controllers/label.py
@@ -5,18 +5,11 @@ from app import app, mongo
 import logger
 from app.schemas.label import validate_label
 from app.services.response import response
-from werkzeug.utils import secure_filename
-import pandas as pd
-
-ALLOWED_EXTENSIONS = set(['csv'])
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
 LOG = logger.get_root_logger(
     __name__, filename=os.path.join(ROOT_PATH, 'output.log'))
     
-def allowed_file(filename):
-  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/all_label', methods=['GET'])
 def get_all_diseases():
   try:
@@ -58,25 +51,7 @@ def add_diseases():
     responses.setMessage("Something wrong :(")
     return jsonify(responses.getResponse())
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-  if 'file' not in request.files:
-    return "no file"
-  
-  file = request.files['file']
-  if file and allowed_file(file.filename):
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    col_names = ['Ambing dan puting bengkak', 'Susus berubah warna', 'Sapi kesakitan bila ambing dipegang', 'Ambing keras dan panas bila dipegang',
-'Sisi perut sebelah kiri membesar','Lambung bila dipukul berbunyi seperti drum','Tidak nafsu makan','Gelisah','Air ludah banyak berbuih dan ngiler',
-'Adanya lepuh lepuh pada gusi,lidah,sekitar kuku dan di ambing susu','Suhu tubuh tinggi','Bengkak di daerah leher,dada,sisi lambung,pinggang dan alat kelamin luar',
-'Pendarahan pada dubur,mulut,hidung, dan urin bercampur darah','Menanduk benda di sekitarnya','Keguguran pada umur kebuntingan 5-8 bulan','Keluar cairan keruh saat keguguran',
-'Produksi susu menurun','Kulit di bawah mata kuning','Abortus atau keguguran terjadi selama1-3 minggu','Urin berwarna merah gelap hampir hitam','Kejang kejang',
-'Terdapat luka di sapi','Menggaruk atau menggesek tubuhnya','Ada kerak atau kopeng pada permukaan kulit','Bulu rontok','label']
-    pima = pd.read_csv("././static/data_dari_koleksi_2.csv", header=None, names=col_names)
-    return pima['label']
-    
-
+   
 # @app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 # def user():
 #     if request.method == 'GET':
