4f0c3b472c735d6c406069c9ea1b97f467b033bf
faruqfadhil <faruqfadhil7@gmail.com>
2019-04-09 20:32:00 +0700

    add csv

--- expert_system/modules/app/controllers/label.py
+++ expert_system/modules/app/controllers/label.py
@@ -6,6 +6,7 @@ import logger
 from app.schemas.label import validate_label
 from app.services.response import response
 from werkzeug.utils import secure_filename
+import pandas as pd
 
 ALLOWED_EXTENSIONS = set(['csv'])
 
@@ -66,7 +67,15 @@ def upload_file():
   if file and allowed_file(file.filename):
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return "success"
+    col_names = ['Ambing dan puting bengkak', 'Susus berubah warna', 'Sapi kesakitan bila ambing dipegang', 'Ambing keras dan panas bila dipegang',
+'Sisi perut sebelah kiri membesar','Lambung bila dipukul berbunyi seperti drum','Tidak nafsu makan','Gelisah','Air ludah banyak berbuih dan ngiler',
+'Adanya lepuh lepuh pada gusi,lidah,sekitar kuku dan di ambing susu','Suhu tubuh tinggi','Bengkak di daerah leher,dada,sisi lambung,pinggang dan alat kelamin luar',
+'Pendarahan pada dubur,mulut,hidung, dan urin bercampur darah','Menanduk benda di sekitarnya','Keguguran pada umur kebuntingan 5-8 bulan','Keluar cairan keruh saat keguguran',
+'Produksi susu menurun','Kulit di bawah mata kuning','Abortus atau keguguran terjadi selama1-3 minggu','Urin berwarna merah gelap hampir hitam','Kejang kejang',
+'Terdapat luka di sapi','Menggaruk atau menggesek tubuhnya','Ada kerak atau kopeng pada permukaan kulit','Bulu rontok','label']
+    pima = pd.read_csv("././static/data_dari_koleksi_2.csv", header=None, names=col_names)
+    return pima['label']
+    
 
 # @app.route('/user', methods=['GET', 'POST', 'DELETE', 'PATCH'])
 # def user():
