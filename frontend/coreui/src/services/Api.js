a542c4d25d7824dc64d28b81161ac64e48c5c2fb
faruqfadhil <faruqfadhil7@gmail.com>
2019-05-03 19:56:04 +0700

    change server IP

--- frontend/coreui/src/services/Api.js
+++ frontend/coreui/src/services/Api.js
@@ -6,15 +6,15 @@ const deviceUrl = function(){
     return 'http://localhost:3001/api/perangkat/'
 }
 const userUrl = function(){
-      return 'http://206.189.36.70:3000/api/user/'
+      return 'http://159.65.6.122:3000/api/user/'
     //return 'http://localhost:3000/api/user/'
 }
 const sapiUrl = function(){
-      return 'http://206.189.36.70:3001/api/sapi/'
+      return 'http://159.65.6.122:3001/api/sapi/'
     //return 'http://localhost:3001/api/sapi/'
 }
 const intelligentUrl = function(){
-    return 'http://206.189.36.70:4000/api/intelligent/'
+    return 'http://159.65.6.122:4000/api/intelligent/'
 }
 module.exports = {
     deviceUrl,
--- sensor_server/services/ConnectRaspi.js
+++ sensor_server/services/ConnectRaspi.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const ConnectRaspi = {
     createInitial: async(id)=>{
-        let result = await axios.post('http://10.8.0.10:3000/raspi/perangkat/create', {
+        let result = await axios.post('http://10.8.0.6:3000/raspi/perangkat/create', {
             idPerangkatOnServer: id
           })
         return result
