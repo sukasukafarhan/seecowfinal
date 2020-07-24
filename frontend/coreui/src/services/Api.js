076f9cc09d6d23b34fb54edc7a7452facffffb8c
sukasukafarhan <farhanmfap2604@gmail.com>
2020-07-24 09:02:14 +0700

    seecow

--- frontend/coreui/src/services/Api.js
+++ frontend/coreui/src/services/Api.js
@@ -4,26 +4,26 @@
  */
 const deviceUrl = function(){
     
-    return 'http://34.101.76.171:3000/api/user/'
+    return 'http://10.184.0.2:3000/api/user/'
     // return 'http://192.168.1.194:3000/api/perangkat/' 
     // return 'http://192.168.1.124:3000/api/perangkat/'
     // return 'http://10.252.175.133:3000/api/perangkat/'
 }
 const userUrl = function(){
-     return 'http://34.101.76.171:3000/api/user/'
+     return 'http://10.184.0.2:3000/api/user/'
     //   return 'http://192.168.1.194:3000/api/user/'
     // return 'http://10.252.175.133:3000/api/user/'
     // return 'http://192.168.1.124:3000/api/user/'
 }
 const sapiUrl = function(){
-    return 'http://34.101.76.171:3001/api/sapi/'
+    return 'http://10.184.0.2:3001/api/sapi/'
     //  return 'http://192.168.1.194:3001/api/sapi/'
     // return 'http://10.252.175.133:3001/api/sapi/'
     // return 'http://192.168.1.124:3001/api/sapi/'
 }
 const intelligentUrl = function(){
     // return 'http://192.168.1.194:4000/api/intelligent/'
-    return 'http://34.101.76.171:4000/api/intelligent/'
+    return 'http://10.184.0.2:4000/api/intelligent/'
     // return 'http://10.252.175.133:4000/api/intelligent/'
     // return 'http://192.168.1.124:4000/api/intelligent/'
 }
